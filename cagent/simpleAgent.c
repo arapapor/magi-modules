60bc2cefd969c7f19b3418bc672de215f6b275eb
arapapor <arapapor@isi.edu>
2016-07-01 11:02:42 -0700

    Update simpleAgent.c

--- cagent/simpleAgent.c
+++ cagent/simpleAgent.c
@@ -125,7 +125,8 @@ int main(int argc, char **argv) {
 	registerFunction("testVoid", "void", &testVoid, 0);
 	registerFunction("addInteger", "int*", &addInteger, 2, "int", "int");
 	registerFunction("subtractInteger", "int*", &subtractInteger, 2, "int", "int");
-	registerFunction("multiplyInteger", "int", &multiplyInteger)
+	registerFunction("multiplyInteger", "int", &multiplyInteger, 2, "int", "int");
+	registerFunction("divideInteger", "int", &divideInteger, 2, "int", "int");
 	//list = ArgParser(argc, argv);
 	agentStart(argc, argv);
 	log_debug(logger, "Going out of the main\n");
