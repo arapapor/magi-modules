0dbe46fdc3d4549cd95849ba7ea5af947a7cff3e
arapapor <arapapor@isi.edu>
2016-07-06 11:44:09 -0700

    Update FileCreator.py

--- FileCreator/FileCreator.py
+++ FileCreator/FileCreator.py
@@ -5,7 +5,7 @@ from magi.util.processAgent import initializeProcessAgent
 class FileCreator(DispatchAgent):
     def __init__(self):
         DispatchAgent.__init__(self)
-        self.filename = 'calc.aal'
+        self.filename = 'users/arapapor/magi-modules/autoCalculator/calc.aal'
 
     # A single method which creates the file named by self.filename.
     # (The @agentmethod() decorator is not required, but is encouraged.
