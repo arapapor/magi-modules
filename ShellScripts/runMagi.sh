d1005797c430886dc6acc8b671691d25d5065217
arapapor <arapapor@isi.edu>
2016-07-08 11:18:14 -0700

    Update runMagi.sh

--- ShellScripts/runMagi.sh
+++ ShellScripts/runMagi.sh
@@ -3,4 +3,4 @@
 echo ~-~-~Running magi_orchestrator.py~-~-~
 # CHANGE THE BELOW PATH
 cd ~/magi-modules/FileCreator
-magi_orchestrator.py -p montage -e rningCalc -f FileCreator.aal
+magi_orchestrator.py -p montage -e calcaar -f FileCreator.aal
