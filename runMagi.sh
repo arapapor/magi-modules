b3cf52a292ab4c544e26555b2bc2331c0c0a13fa
arapapor <arapapor@isi.edu>
2016-07-09 22:48:17 -0700

    Update runMagi.sh

--- runMagi.sh
+++ runMagi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Start magi_orchestrator.py
+base="~/magi-modules/"
+echo -----input filename-----
+read path
+echo -----input file-----
+read fname
+echo -----input expirement-----
+read exp
 echo ~-~-~Running magi_orchestrator.py~-~-~
 # CHANGE THE BELOW PATH
-cd ~/magi-modules/FileCreator
-magi_orchestrator.py -p montage -e calcaar -f FileCreator.aal
+cd $base$path
+magi_orchestrator.py -p montage -e $exp -f $fname
