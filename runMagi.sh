4a13015474b4feefae1477bd6ddbd9269fb850ca
arapapor <arapapor@isi.edu>
2016-07-11 12:22:22 -0700

    Update runMagi.sh

--- runMagi.sh
+++ runMagi.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 # Start magi_orchestrator.py
 base="~/magi-modules/"
-'''
-echo -----input filename-----
-read path
-echo -----input file-----
-read fname
-echo -----input expirement-----
-read exp
-'''
-#read path read fname read exp
 
-while read x read y read z; do
-  echo $x$y$z
-done <~/runMagi.txt
+echo ~-~-~Reading form runMagi.txt~-~-~
+read path read fname read exp <~/runMagi.txt
+
+echo $path $fname $exp
+
+echo ~-~-~Running magi_orchestrator.py~-~-~
+# CHANGE THE BELOW PATH
+cd $base$path
+magi_orchestrator.py -p montage -e $exp -f $fname
+
 
 echo ~-~-~Running magi_orchestrator.py~-~-~
 # CHANGE THE BELOW PATH
