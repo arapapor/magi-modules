19e5fb8282b5f436bd08ade9c831ac8b073669f1
arapapor <arapapor@isi.edu>
2016-07-11 12:20:06 -0700

    Create runInfo.sh

---
+++ runInfo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+basepath="magi-modules/"
+echo "-~-input: Folder - File Name - Expirement-~-"
+dash=" - "
+read path read fname read exp
+echo $path$dash$fname$dash$exp >  ~/runMagi.txt
+
