08e92be17f0af7f753751733ce60ca98f4e67486
arapapor <arapapor@isi.edu>
2016-07-11 10:39:06 -0700

    Update runMagi.sh

--- runMagi.sh
+++ runMagi.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Start magi_orchestrator.py
 base="~/magi-modules/"
+'''
 echo -----input filename-----
 read path
 echo -----input file-----
 read fname
 echo -----input expirement-----
 read exp
+'''
+read path read fname read exp
 echo ~-~-~Running magi_orchestrator.py~-~-~
 # CHANGE THE BELOW PATH
 cd $base$path
