5237372a51665375a0fe779bb85737946865524d
arapapor <arapapor@isi.edu>
2016-07-11 12:23:56 -0700

    Update updateGit.sh

--- updateGit.sh
+++ updateGit.sh
@@ -3,10 +3,9 @@
 echo ~-~-~Deleting Magi-Modules~-~-~
 rm -r -f magi-modules
 
-cp magi-modules/updateGit.sh ~/
-cp magi-modules/runMagi.sh ~/
-cp magi-modules/runMagi.txt ~/
-
 git clone https://github.com/arapapor/magi-modules
 cd
 
+cp magi-modules/updateGit.sh ~/
+cp magi-modules/runMagi.sh ~/
+cp magi-modules/runMagi.txt ~/
