f6d4e1d2c541ceaca510275a14d5f63325375ae2
arapapor <arapapor@isi.edu>
2016-07-20 15:37:40 -0700

    Update Building.py

--- SmartGridSchool/Building.py
+++ SmartGridSchool/Building.py
@@ -2,10 +2,9 @@
 from magi.util.agent import DispatchAgent, agentmethod
 from magi.util.processAgent import initializeProcessAgent
 '''
-
 # commented out code: don't know if necessary/not yet implemented
 import logging
-from math import sin, cos, asin, acos
+from math import sin, cos, asin, acos, tan, atan
 import math
 # import scipy.io
 import sys
@@ -22,29 +21,56 @@ import yaml
 class Building():  # difference between DispatchAgent and NonBlockingDispatchAgent?
     def __init__(self):
         object.__init__(self)
+        
+        #constant for earth
+        self.obl = 23.44 * (math.pi / 180)
+
+        #data recieved from array config
         self.panelEff = 0.17
         self.panelArea = 20
-        self.ELA = 35 * (math.pi / 180)
+        self.ELA = 25.0 * (math.pi / 180.0)
         self.AZA = math.pi
 
-        self.day = 1
+        #data recieved by the simulation configuration
+        self.day = 171
+        #iterative
         self.LT = 0
-        self.B = (math.pi / 180) * (360 / 365) * (self.day - 81)
-        self.EoT = 9.87 * sin(2 * self.B) - 7.53 * cos(self.B) - 1.5 * sin(self.B)
-        self.delta = 23.45 * sin(self.B)
-        self.LAT = 33.77984
+
+        #calculations done for each day of simulation
+        self.B = (math.pi / 180.0) * (360.0 / 365.0) * (self.day - 10)
+        self.EoT = 9.87 * sin(2.0 * self.B) - 7.53 * cos(self.B) - 1.5 * sin(self.B)
+        self.delta = (math.pi / 180.0) * asin(sin(self.obl) * sin(self.B))
+
+        #constant latitude of Chadwick
+        self.LAT = 33.77984 * (math.pi / 180.0)
+
 
 
     # electricity generation
     def generation(self, msg):
-        self.LST = self.LT + (4 * (-13.359) + self.EoT) / 60
-        self.HRA = (math.pi / 180) * 15 * (self.LST - 12)
+        
+        #calculations done for each iteration 
+        self.LST = self.LT + (4.0 * (-13.361) + self.EoT) / 60.0
+        self.HRA = (math.pi / 180.0) * (15.0 * (self.LST - 12))
+        
         self.SELA = asin(sin(self.delta) * sin(self.LAT) + cos(self.delta) * cos(self.LAT) * cos(self.HRA))
-        self.SAZA = acos(sin(self.delta) * cos(self.LAT) + cos(self.delta) * sin(self.LAT) * cos(self.HRA) / cos(self.SELA))
+        self.SAZA = acos((sin(self.delta) * cos(self.LAT) - cos(self.delta) * sin(self.LAT) * cos(self.HRA)) / cos(self.SELA))  #cos(self.SELA)
         self.declination = (1 - self.SELA)
-        self.solarIrradiance = pow(1.353, pow(pow(0.7, (1 / 1e-4) + cos(1 / self.declination)), 0.678))
+        
+        #done only if the array is a tracking array 
+        '''building needs to parce for an argument called "tracking" or something of the kind'''
+        if self.tracking:
+            self.ELA = self.SELA
+            self.AZA = self.SAZA
+        
+        #implement solar irradiance equation, 
+        '''in self.gen 1.5 refers to the general maximum power of the solar irradiance cast over one square meter of earth'''
+
+        self.solarIrradiance = pow(1.353, pow(pow(0.7, (1.0 / 1e-4) + cos(1.0 / self.declination)), 0.678))
         self.solarIrradiance *= (cos(self.SELA) * sin(self.ELA) * cos(self.AZA - self.SAZA) + sin(self.SELA) * cos(self.ELA))
         self.gen = self.solarIrradiance * (self.panelEff * 1.5) * self.panelArea
+        
+        #debugging loggers        
         '''
         print(self.HRA)
         print(self.SELA)
@@ -52,71 +78,49 @@ class Building():  # difference between DispatchAgent and NonBlockingDispatchAge
         print(self.declination)
         print(self.solarIrradiance)
         '''
-        return -self.gen
-
-    # electricity generation
-    def generationMoving(self, msg, direction):
-        self.LST = self.LT + (4 * (-13.359) + self.EoT) / 60
-        self.HRA = (math.pi / 180) * 15 * (self.LST - 12)
-        self.SELA = asin(sin(self.delta) * sin(self.LAT) + cos(self.delta) * cos(self.LAT) * cos(self.HRA))
-        self.SAZA = acos(sin(self.delta) * cos(self.LAT) + cos(self.delta) * sin(self.LAT) * cos(self.HRA) / cos(self.SELA))
-
-
-
-        self.declination = (1 - self.SELA)
-        self.solarIrradiance = pow(1.353, pow(pow(0.7, (1 / 1e-4) + cos(1 / self.declination)), 0.678))
-
-        if direction == 0:
-            self.ELA = self.SELA
-            self.AZA = self.SAZA
-            self.solarIrradiance *= (cos(self.SELA) * sin(self.ELA) * cos(self.AZA - self.SAZA) + sin(self.SELA) * cos(self.ELA))
-        elif direction == 1:
-            self.ELA = self.SELA
-            self.solarIrradiance *= (cos(self.SELA) * sin(self.ELA) * cos(self.AZA - self.SAZA) + sin(self.SELA) * cos(self.ELA))
-
-        elif direction == 2:
-            self.AZA = self.SAZA
-            self.solarIrradiance *= (cos(self.SELA) * sin(self.ELA) * cos(self.AZA - self.SAZA) + sin(self.SELA) * cos(self.ELA))
-
-        self.gen = self.solarIrradiance * (self.panelEff * 1.5) * self.panelArea
-
-        return -self.gen
-
-
+        return self.gen
 
     # electricity consumption
     def consumption(self, msg):
+        #this operation is to satisfy the consumption functions need of at least one directive, because we have no implemented models for consumption yet
         self.c = 1
 
 
 if __name__ == "__main__":
     room = Building()
-    room1 = Building()
-    room2 = Building()
     power = 0
-    power1 = 0
-    power2 = 0
-    for i in range(420, 1140):
-        room.LT = i / 60.0
-        # print(room.LT)
-        power += room.generationMoving(room, 0) / 60
 
-    for j in range(420, 1140):
-        room.LT = i / 60.0
-        power1 += room1.generationMoving(room1, 1) / 60
+    #sunrise and sunset calculator, will need a stream related implementation
+    rise = -tan(room.LAT) * tan(room.delta)
+    set = tan(room.LAT) * tan(room.delta)
 
-    for k in range(420, 1140):
-        room.LT = i / 60.0
-        power2 += room2.generationMoving(room2, 2) / 60
+    rise = (math.pi / 2) + rise
+    set = (3 * math.pi / 2) + set
 
+    #converts to minutes / 1440 (per day)
+    rise /= 2 * math.pi
+    set /= 2 * math.pi
+
+    rise *= 1440
+    set *= 1440
+
+    rise = int(rise)
+    set = int(set)
+
+    #print(str(rise))
+    #print(str(set))
+
+    rise = int(rise)
+    set = int(set)
+
+    for i in range(rise + 1, set - 1):
+        room.LT = i / 60.0
+        #print(room.LT)
+        power += room.generation(room) / 60.0
+        #print str(i / 60.0)
+    #prints the Power generation for the day in Kilowatt*Hours
     print(str(power))
     print("")
-    print(str(power1))
-    print("")
-    print(str(power2))
-
-
-
 
 
 
